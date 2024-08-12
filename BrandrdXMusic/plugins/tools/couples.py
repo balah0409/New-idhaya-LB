b5d741c5d766acd34ff88dcce001ea14fc6946c8
balah0409 <156516578+balah0409@users.noreply.github.com>
2024-08-12 10:16:22 +0530

    Update couples.py

--- BrandrdXMusic/plugins/tools/couples.py
+++ BrandrdXMusic/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from BrandrdXMusic.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="ᴍʏ ᴄᴜᴛᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ  🥀",
-            url=f"https://t.me/BRANDED_PAID_CC",
+            text="ᴄʜᴀᴛ",
+            url=f"https://t.me/musicheavenvibes",
         ),
     ],
 ]
